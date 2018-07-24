35497a86b032ae287a03986827cd6134e6959c8f
oitvrdev <oit-vrdev@wmich.edu>
2018-07-24 12:02:56 -0400

    Blood Admin Checklist Started - Kevin

---
+++ Assets/BloodAdminChecklist.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodAdminChecklist : MonoBehaviour {
+
+    public int currentPage;
+
+    public List<string> questions;
+
+    public bool medicalOrder;
+
+    public List<int> patientID;
+    
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
