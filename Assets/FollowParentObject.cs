84ca9c866af0a689a653d5dd426cfe3b903ebf4f
oitvrdev <oit-vrdev@wmich.edu>
2018-07-24 11:02:37 -0400

    Hand Sanitizer Particles Functions Complete (needs final particles) - Kevin

---
+++ Assets/FollowParentObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowParentObject : MonoBehaviour {
+
+    public Transform myParent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (myParent != null)
+        {
+            transform.position = myParent.transform.position;
+        }
+    }
+}
--- Assets/Scripts/New Item Interactions/HandSanitizer.cs
+++ Assets/Scripts/New Item Interactions/HandSanitizer.cs
@@ -4,14 +4,15 @@ using System.Security.Authentication;
 using Obi;
 using UnityEngine;
 
-public class HandSanitizer : ItemInteraction {
+public class HandSanitizer : ItemInteraction
+{
 
     public AudioClip dispenseClip;
     public AudioClip contactClip;
 
     public GameObject sanitizerDollop;
     public Transform dispensePoint;
-   
+
     public GameObject rightHand, leftHand;
     public Collider saniCollider;
 
@@ -20,71 +21,101 @@ public class HandSanitizer : ItemInteraction {
     public bool rightSanitized;
     private string activeHand;
 
+    public GameObject colliderCube;
+    private GameObject newCCube;
     public ParticleSystem sanitizerParticles;
 
     public override void Grabbed()
     {
         base.Grabbed();
     }
-    
+
+
+
+
     private void OnTriggerEnter(Collider other)
-    {        
-       // other.isTrigger = true;
-        other.gameObject.AddComponent<SaniCollideScript>();
+    {
+        // other.isTrigger = true;
 
-            if (other.transform.parent.transform.parent.name == "RightController")
-            {
-                activeHand = "Right";
-            }
 
-            else if (other.transform.parent.transform.parent.name == "LeftController")
-            {
-                activeHand = "Left";
-                /*  if (saniCollider == null)
-                  {
-                      saniCollider = rightHand.AddComponent<SphereCollider>();
-                      saniCollider.isTrigger = true;
-                      rightHand.AddComponent<SaniCollideScript>();
-                      Debug.Log("Right Hand Collider Created");
-                  }*/
-            }
+        //Debug.Log(other.gameObject.name);
+
+        if (other.gameObject.name == "Sphere")
+        {
+            ParticleSystem.CollisionModule pCollision = sanitizerParticles.collision;
+            newCCube = Instantiate(colliderCube);
+            newCCube.GetComponent<FollowParentObject>().myParent = other.transform;
+            pCollision.SetPlane(0, newCCube.transform);
+
+        }
+
+        //if (other.gameObject.GetComponent<SaniCollideScript>() == null)
+        //{
+        //    other.gameObject.AddComponent<SaniCollideScript>();
+        //}
+
+        if (other.transform.parent.transform.parent.name == "RightController")
+        {
+            activeHand = "Right";
+            Dispense();
+        }
+
+        else if (other.transform.parent.transform.parent.name == "LeftController")
+        {
+            activeHand = "Left";
+            Dispense();
+
+        }
+
         
-        Dispense(); 
     }
 
     private void OnTriggerStay(Collider other)
     {
-        dispensing = true;
+        // dispensing = true;
     }
 
 
     private void OnTriggerExit(Collider other)
     {
         dispensing = false;
+        sanitizerParticles.Stop();
+        if (newCCube != null)
+        {
+            Destroy(newCCube);
+        }
     }
 
-    public void Dispense() {
+    public void Dispense()
+    {
 
         if (!dispensing)
         {
             dispensing = true;
-            GameObject thisDollop = Instantiate(sanitizerDollop, dispensePoint.position, Quaternion.identity);
-
+        //    GameObject thisDollop = Instantiate(sanitizerDollop, dispensePoint.position, Quaternion.identity);
 
             sanitizerParticles.Play();
-
 
             if (activeHand == "Left")
             {
                 leftSanitized = true;
                 Debug.Log("Left Sani");
+                if (leftSanitized && rightSanitized)
+                {
+                    Sanitized();
+                }
             }
-            
+
             if (activeHand == "Right")
             {
                 rightSanitized = true;
                 Debug.Log("Right Sani");
+                if (leftSanitized && rightSanitized)
+                {
+                    Sanitized();
+                }
             }
+
             thisAudio.clip = dispenseClip;
             thisAudio.Play();
         }
@@ -94,11 +125,12 @@ public class HandSanitizer : ItemInteraction {
     {
         thisAudio.clip = contactClip;
         thisAudio.Play();
+        Debug.Log("both Sani");
         if (leftSanitized && rightSanitized)
         {
             Task.CheckTasks(true);
         }
-        
-       
+
+
     }
 }
